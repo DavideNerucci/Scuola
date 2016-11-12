fb0bbccc4f50b4df217caf1674f01ceb16254358
Davide Nerucci <Davide Nerucci>
2016-11-12 12:42:04 +0100

    1

---
+++ Class1.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eserciziothread1;
+
+import static java.lang.Thread.sleep;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author davide
+ */
+public class Class1 extends Thread
+{
+    private  int[]v;
+
+    public Class1(int[] vettore) {
+        this.v = vettore;
+    }
+    
+    public int[] genera(int[] v)
+    {
+        for(int i=0;i<this.v.length;i++)
+        {
+            v[i]= (int) (Math.random()*100);
+        }
+        return v;
+    }
+    
+    public int[]ordina(int[]v)
+    {
+        for(int i=0;i<this.v.length;i++)
+        {
+            for(int k=0;k<this.v.length;k++)
+            {
+                if(v[i]<v[k])
+                {
+                    int app=v[i];
+                    v[i]=v[k];
+                    v[k]=app;
+                }
+            }
+        }
+        return v;
+    }
+    @Override
+    public void run()
+    {
+       genera(this.v); 
+        try 
+        {
+            sleep(200);
+        }catch (InterruptedException ex){} 
+       
+        ordina(this.v);   
+       
+        try {
+            join(400);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Class1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+       for(int i=0;i<this.v.length;i++)
+       {
+           System.out.println("Vettore-1 : "+this.v[i]);
+       }
+    }
+}
